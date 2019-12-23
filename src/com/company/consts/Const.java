82b2092b62dbb76b52bf21d5945c8c42537bf284
necto128 <nester1012000@mail.ru>
2019-12-23 04:49:18 +0300

    11

---
+++ src/com/company/consts/Const.java
@@ -0,0 +1,16 @@
+package com.company.consts;
+
+public class Const {////+++
+    protected String dbhost="localhost";
+    protected String dbport="3306";
+    protected static String dbuser="root";
+    protected static String dbpass="1";
+    protected String dbname="catworld";
+
+    public static final String USER_TABLE = "cats";
+    public static final String ID_CAT ="id";
+    public static final String NAME_CAT ="name_cat";
+    public static final String ID_DAD ="id_dad";
+    public static final String ID_MAM ="id_mam";
+    public static final String GENDER ="gender";
+}
