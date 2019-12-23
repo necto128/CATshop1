6a253e8c8c27b6dc19a9e703b3bd0ad8153505ad
necto128 <nester1012000@mail.ru>
2019-12-23 04:52:44 +0300

    11

--- src/com/company/consts/Const.java
+++ src/com/company/consts/Const.java
@@ -1,6 +1,6 @@
 package com.company.consts;
 
-public class Const {////+++
+public class Const {////+++frtgyhujikm,l.;'
     protected String dbhost="localhost";
     protected String dbport="3306";
     protected static String dbuser="root";
